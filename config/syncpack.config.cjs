b8168c2dd739f5e0c9c514b7a6e701d0118f3960
FlavioScimeca <flavioscimeca3@gmail.com>
2025-04-06 20:02:40 +0200

    restore /apps

---
+++ config/syncpack.config.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  source: [
+    'package.json',
+    'apps/*/package.json',
+    'packages/*/package.json',
+    'server/package.json',
+  ],
+
+  semverRanges: ['^', '~'],
+  indent: '  ',
+  sortAz: [
+    'contributors',
+    'dependencies',
+    'devDependencies',
+    'keywords',
+    'peerDependencies',
+    'scripts',
+  ],
+  sortFirst: ['name', 'version', 'private', 'description', 'author', 'license'],
+};
