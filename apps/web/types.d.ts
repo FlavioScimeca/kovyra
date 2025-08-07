bb73385cbcc5534d5f21453fdcf32b9bc541cb50
FlavioScimeca <flavioscimeca3@gmail.com>
2025-08-08 00:52:57 +0200

    removed next

--- apps/web/types.d.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/no-empty-object-type*/
-
-import type { config } from '@kovyra/theme';
-
-export type Conf = typeof config;
-
-declare module '@kovyra/theme' {
-  interface TamaguiCustomConfig extends Conf {}
-}
